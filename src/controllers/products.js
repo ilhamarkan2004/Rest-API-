866aa4d773413c796e80dd965950f94cfb1fa49e
ilhamarkan2004 <ilhamarkan2004@gmail.com>
2023-12-08 22:31:26 +0700

    Request Body

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,10 +1,12 @@
 exports.createProduct = (req, res, next) => {
+  const name = req.body.name;
+  const price = req.body.price;
   res.json({
     message: "Create product success",
     data: {
       id: 1,
-      name: "Botol",
-      price: 2000,
+      name: name,
+      price: price,
     },
   });
   next();
