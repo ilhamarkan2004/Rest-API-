9fa84483755df9416ec761340ffb659419729f95
ilhamarkan2004 <ilhamarkan2004@gmail.com>
2023-12-08 22:20:53 +0700

    CORS ORIGIN

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const app = express();
 
-const productRoute = require("./src/routes/products")
+const productRoute = require("./src/routes/products");
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,POST,PUT,PATCH,DELETE,OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  next();
+});
 
 app.use("/v1/customer", productRoute);
 
